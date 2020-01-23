a9cc6f5c54f4a9f66894c6c620c502c9c0068e6f
danielsampar12 <54335160+danielsampar12@users.noreply.github.com>
2020-01-23 18:39:00 -0300

    adicionando as bibliotecas csvtojson

--- backend/src/readLine.js
+++ backend/src/readLine.js
@@ -1,10 +1,15 @@
 //const csv = require('csv-parser');
 const CSVtoJSON = require('csvtojson');
 const JSONtoCSV = require('json2csv').parse; 
+const routes = require('./routes');
+const Empresa = require('./models/Empresa');
 const fs = require('fs');
 const csvData = [];
 
-CSVtoJSON().fromFile('../data/clientes-data.csv');
+CSVtoJSON().fromFile('../data/clientes-data.csv').then(data => {
+  console.log(data);
+  
+});
 
 /*
 fs.createReadStream('c:/csv-maker/backend/data/clientes-data.csv')
