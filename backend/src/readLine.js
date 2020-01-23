b0d825ab71568a6d999eee4bf225d545d17ee1dd
danielsampar12 <daniel_sampar@hotmail.com>
2020-01-23 21:38:53 -0200

    algumas alreações estranhas

--- backend/src/readLine.js
+++ backend/src/readLine.js
@@ -3,16 +3,22 @@ const CSVtoJSON = require('csvtojson');
 const JSONtoCSV = require('json2csv').parse; 
 const routes = require('./routes');
 const Empresa = require('./models/Empresa');
+const csv = require('csv-parser');
 const fs = require('fs');
 const csvData = [];
-
+/*
 CSVtoJSON().fromFile('../data/clientes-data.csv').then(data => {
-  console.log(data);
+  try {
+    console.log(data);
+  } catch (error) {
+    console.log('Acabou o arquivo');
+  }
+  
   
 });
+*/
 
-/*
-fs.createReadStream('c:/csv-maker/backend/data/clientes-data.csv')
+fs.createReadStream('../data/clientes-data.csv')
   .pipe(csv())
   .on('data', (row) => {
     console.log(row)
@@ -23,4 +29,4 @@ fs.createReadStream('c:/csv-maker/backend/data/clientes-data.csv')
   });
 
   console.log(csvData);
-  */
+
